540a2c041e32d378fba82236676a66b02f174929
akaibashi <titou5273@gmail.com>
2016-09-18 17:23:30 +0900

    test controller commit

--- sound_cloud_play/config/routes.rb
+++ sound_cloud_play/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'tests/index'
+
+  get 'tests/show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
